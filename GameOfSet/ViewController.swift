01afbc04e8e09d98468ecbb5cf774da7782e98c8
Lien Ly <liely@deloitte.com>
2017-11-29 09:55:05 -0700

    Initial Commit

---
+++ GameOfSet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GameOfSet
+//
+//  Created by Ly, Lien (US - Denver) on 11/29/17.
+//  Copyright © 2017 Ly, Lien (US - Denver). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
