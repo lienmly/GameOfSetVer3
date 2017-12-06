a2926e80b96b2db58eed11cb1f959b459241f116
Lien Ly <liely@deloitte.com>
2017-12-06 11:04:24 -0700

    Setup basic display files

---
+++ GameOfSet/AllCardsView.swift
@@ -0,0 +1,15 @@
+//
+//  AllCardsView.swift
+//  GameOfSet
+//
+//  Created by Ly, Lien (US - Denver) on 12/6/17.
+//  Copyright © 2017 Ly, Lien (US - Denver). All rights reserved.
+//
+
+import UIKit
+
+class AllCardsView: UIView {
+
+   
+
+}
---
+++ GameOfSet/CardView.swift
@@ -0,0 +1,32 @@
+//
+//  CardView.swift
+//  GameOfSet
+//
+//  Created by Ly, Lien (US - Denver) on 12/5/17.
+//  Copyright © 2017 Ly, Lien (US - Denver). All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+    var number: Card.Number = .one { didSet { setNeedsDisplay(); setNeedsLayout() } }
+    var symbol: Card.Symbol = .one { didSet { setNeedsDisplay(); setNeedsLayout() } }
+    var shading: Card.Shading = .one { didSet { setNeedsDisplay(); setNeedsLayout() } }
+    var color: Card.Color = .one { didSet { setNeedsDisplay(); setNeedsLayout() } }
+    
+    override func draw(_ rect: CGRect) {
+        let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: 16.0)
+        roundedRect.addClip()
+        UIColor.white.setFill()
+        roundedRect.fill()
+    }
+}
+
+extension CardView {
+    private struct Constants {
+        static let cornerRadiusToBoundsHeight: CGFloat = 0.06
+    }
+    private var cornerRadius: CGFloat  {
+        return bounds.size.height * Constants.cornerRadiusToBoundsHeight
+    }
+}
